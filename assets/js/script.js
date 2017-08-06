57288e789b9c4e4db4c3dc4ea3d4154db9e8a284
ricardo <ricardoj@gec.inatel.br>
2017-08-06 15:28:36 -0300

    Primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Created by ricardo on 29/07/17.
+ */
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        direction: 'horizontal',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        keyboardControl: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+
+        grabCursor: true,
+
+    });
+
+
+
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
